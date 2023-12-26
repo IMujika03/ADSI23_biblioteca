189a150522f7576d4b7083c14bc57aae66ad4fa5
Markel15 <enemar.h@gmail.com>
2023-12-26 17:32:26 +0100

    Arreglo al iniciar sesion pertsonala y test pertsonala

--- tests/test_profila.py
+++ tests/test_profila.py
@@ -3,8 +3,32 @@ from bs4 import BeautifulSoup
 
 class TestProfila(BaseTestClass):
 
-    def test_profila_ikusi(self):
+    def test_ikusi_erreserbak(self):
         self.login('james@gmail.com', '123456')
         res = self.client.get('/pertsonala')
         self.assertEqual(200, res.status_code) #Orriak ondo funtzionatzen du bertara heltzen delako
         page = BeautifulSoup(res.data, features="html.parser")
+        title = page.select_one('h5.card-title a').getText()
+        self.assertEqual('El oro y la ceniza', title)#Erabiltzaileak liburu hau erreserbaturik du
+        mezua = page.find(string="Ez daude libururik erreserbaturik.")
+        self.assertNotEqual('Ez daude libururik erreserbaturik.', mezua)# Ez da agertzen errore mezurik
+
+    def test_ikusi_ez_duela_erreseinarik(self):
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/pertsonala')
+        page = BeautifulSoup(res.data, features="html.parser")
+        valoracion = page.select_one('div.valoracion').getText()
+        self.assertIn('Ez da egin erreseinarik', valoracion)#Liburuak erreseinarik ez duelako errore mezua agertzen da
+    def test_ikusi_erreseina(self):
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/pertsonala')
+        page = BeautifulSoup(res.data, features="html.parser")
+        valoraciones = page.select('div.valoracion')
+        erreseina = valoraciones[2].getText().strip().replace('\n', '')#hutsune zuriak ezabatu konprobazioa ondo egiteko
+        self.assertEqual('Liburu ona, baina, pertsonaiak txarrak', erreseina)#Erreseinak ondo agertzen direla ikusten da
+    def test_ikusi_erreserbarik_ez(self): #Funciona bien, pero, solo si se ejecutan de uno en uno
+        self.login('ejemplo2@gmail.com', '123456')
+        res2 = self.client.get('/pertsonala')
+        page2 = BeautifulSoup(res2.data, features="html.parser")
+        mezua = page2.find(string="Ez daude libururik erreserbaturik.")
+        self.assertEqual('Ez daude libururik erreserbaturik.', mezua)#Erreserbarik ez daudelakoe errore mezua ikusten da
