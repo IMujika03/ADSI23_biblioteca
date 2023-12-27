08bfb136ab077fe9cb5db767cca82234d6bee5ee
Markel15 <enemar.h@gmail.com>
2023-12-27 02:24:15 +0100

    Test eskaerak primer

--- tests/test_aukerak.py
+++ tests/test_aukerak.py
@@ -13,7 +13,7 @@ class TestAukerak(BaseTestClass):
         mezua = page.find(string='Momentu honetan ez dituzu onartzen lagunak, kontrakoa nahi baduzu, pultsatu botoia')
         self.assertEqual('Momentu honetan ez dituzu onartzen lagunak, kontrakoa nahi baduzu, pultsatu botoia', mezua)#botoia pultsatu baino lehen aukera ezgaituta zegoen
         res2 = self.client.post('/aukerak', data={'aldatu1era': 'Lagunak onartu'}) # Aukera aldatzeko botoia pulsatu
-        self.assertEqual(200, res.status_code)  # Eskakizuna ondo egin dela konprobatu
+        self.assertEqual(200, res2.status_code)  # Eskakizuna ondo egin dela konprobatu
         page2 = BeautifulSoup(res2.data, features="html.parser")#orria aldatu da, beraz, aldagaia berriztu behar da
         mezua = page2.find(string='Momentu honetan lagunak onartzen dituzu, kontrakoa nahi baduzu, pultsatu botoia')# mezua aldatu dela konprobatu
         self.assertEqual('Momentu honetan lagunak onartzen dituzu, kontrakoa nahi baduzu, pultsatu botoia',
--- tests/test_eskaerak.py
+++ tests/test_eskaerak.py
@@ -4,5 +4,17 @@ from bs4 import BeautifulSoup
 class TestEskaerak(BaseTestClass):
     def test_onartu(self):
         self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'james@gmail.com'")#hasieratu egoera berdinera
-
-
+        self.db.delete(f"DELETE FROM LagunEgin")#Aurretik zegoen informazioa ezabatu test-ak errepikatu ahal izateko
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/eskaerak')
+        self.assertEqual(200, res.status_code)  # Orriak ondo funtzionatzen du bertara heltzen delako
+        page = BeautifulSoup(res.data, features="html.parser")
+        erabiltzailea = page.select_one('h6').getText()
+        self.assertEqual('jhon@gmail.com',erabiltzailea)# Berez, lagunakOnartzekoAukera gaituta duen bakarra
+        res2 = self.client.post('/eskaerak', data={'onartu': 'onartu', 'korreoa': 'jhon@gmail.com'})
+        self.assertEqual(200, res2.status_code)#Ondo egin da eskakizuna
+        page2 = BeautifulSoup(res2.data, features="html.parser")  # orria aldatu da
+        mezua = page2.find(string=' Ez daude erabiltzailerik lagunak izateko')
+        self.assertEqual(' Ez daude erabiltzailerik lagunak izateko', mezua) #Eskaera kendu da
+        res3 = self.db.select(f"SELECT Erabiltzailea2 FROM LagunEgin WHERE Erabiltzailea1 = 'james@gmail.com'")
+        self.assertEqual('jhon@gmail.com',res3[0][0])
