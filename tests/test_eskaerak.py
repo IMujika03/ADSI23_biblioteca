478f67c27a09d9c144d13d5bfb51ea262de55a22
Markel15 <enemar.h@gmail.com>
2023-12-27 20:21:55 +0100

    Amaitu test

--- tests/test_eskaerak.py
+++ tests/test_eskaerak.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 class TestEskaerak(BaseTestClass):
     def test_onartu(self):
         self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'james@gmail.com'")#hasieratu egoera berdinera
+        self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'jhon@gmail.com'")
         self.db.delete(f"DELETE FROM LagunEgin")#Aurretik zegoen informazioa ezabatu test-ak errepikatu ahal izateko
         self.login('james@gmail.com', '123456')
         res = self.client.get('/eskaerak')
@@ -22,6 +23,7 @@ class TestEskaerak(BaseTestClass):
     def test_ezeztatu(self):
         self.db.update(
             f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'james@gmail.com'")  # hasieratu egoera berdinera
+        self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'jhon@gmail.com'")
         self.db.delete(f"DELETE FROM LagunEgin")  # Aurretik zegoen informazioa ezabatu test-ak errepikatu ahal izateko
         self.login('james@gmail.com', '123456')
         res = self.client.get('/eskaerak')
@@ -37,6 +39,7 @@ class TestEskaerak(BaseTestClass):
     def test_onartu_bere_burua(self): #Hau ezin da egin web orrian, ez delako aukerarik ematen
         self.db.update(
             f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 0 WHERE MailKontua = 'james@gmail.com'")
+        self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'jhon@gmail.com'")
         self.db.delete(f"DELETE FROM LagunEgin")
         self.login('james@gmail.com', '123456')
         res = self.client.get('/eskaerak')
@@ -49,9 +52,10 @@ class TestEskaerak(BaseTestClass):
     def test_onartu_erab_bera(self): #Ez da aukerarik ematen web orrian laguna den erabiltzaile bat berriro onartzeko, baina, test-a egindo da
         self.db.update(
             f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'james@gmail.com'")
+        self.db.update(f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'jhon@gmail.com'")
         self.db.delete(f"DELETE FROM LagunEgin")
         self.login('james@gmail.com', '123456')
-        res2 = self.client.post('/eskaerak', data={'onartu': 'onartu', 'korreoa': 'jhon@gmail.com'})
+        self.client.post('/eskaerak', data={'onartu': 'onartu', 'korreoa': 'jhon@gmail.com'})
         res3 = self.db.select(f"SELECT Egoera FROM LagunEgin WHERE Erabiltzailea1 = 'james@gmail.com'")
         self.assertEqual(1, res3[0][0])  # 1 da, onartu delako
         self.assertEqual(1, len(res3)) #Bakarrik agertzen da behin
@@ -61,3 +65,12 @@ class TestEskaerak(BaseTestClass):
         self.assertEqual(1, len(res3))  # Bakarrik agertzen da behin, ez da berriro gehitu
 
     def test_onartzeko_erabiltzailerik_ez(self):
+        self.db.update(
+            f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 0 WHERE MailKontua = 'jhon@gmail.com'")
+        self.db.delete(f"DELETE FROM LagunEgin")
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/eskaerak')
+        page = BeautifulSoup(res.data, features="html.parser")
+        mezua = page.find(string=' Ez daude erabiltzailerik lagunak izateko')
+        self.assertEqual(' Ez daude erabiltzailerik lagunak izateko', mezua) #Errore mezua agertzen da
+
