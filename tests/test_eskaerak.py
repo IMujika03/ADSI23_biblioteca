8d4c4c2213a36a8ddf1ac44768a3e417748c4f81
Markel15 <enemar.h@gmail.com>
2023-12-27 15:49:03 +0100

    Arreglar fechas en erreserbatuta

--- tests/test_eskaerak.py
+++ tests/test_eskaerak.py
@@ -16,5 +16,22 @@ class TestEskaerak(BaseTestClass):
         page2 = BeautifulSoup(res2.data, features="html.parser")  # orria aldatu da
         mezua = page2.find(string=' Ez daude erabiltzailerik lagunak izateko')
         self.assertEqual(' Ez daude erabiltzailerik lagunak izateko', mezua) #Eskaera kendu da
-        res3 = self.db.select(f"SELECT Erabiltzailea2 FROM LagunEgin WHERE Erabiltzailea1 = 'james@gmail.com'")
-        self.assertEqual('jhon@gmail.com',res3[0][0])
+        res3 = self.db.select(f"SELECT Egoera FROM LagunEgin WHERE Erabiltzailea1 = 'james@gmail.com'")
+        self.assertEqual(1, res3[0][0])  # 1 da, onartu delako
+
+    def test_ezeztatu(self):
+        self.db.update(
+            f"UPDATE Erabiltzailea SET lagunakOnartzekoAukera = 1 WHERE MailKontua = 'james@gmail.com'")  # hasieratu egoera berdinera
+        self.db.delete(f"DELETE FROM LagunEgin")  # Aurretik zegoen informazioa ezabatu test-ak errepikatu ahal izateko
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/eskaerak')
+        page = BeautifulSoup(res.data, features="html.parser")
+        res2 = self.client.post('/eskaerak', data={'ezeztatu': 'ezeztatu', 'korreoa': 'jhon@gmail.com'})
+        self.assertEqual(200, res2.status_code)  # Ondo egin da eskakizuna
+        page2 = BeautifulSoup(res2.data, features="html.parser")  # orria aldatu da
+        mezua = page2.find(string=' Ez daude erabiltzailerik lagunak izateko')
+        self.assertEqual(' Ez daude erabiltzailerik lagunak izateko', mezua)  # Eskaera kendu da
+        res3 = self.db.select(f"SELECT Egoera FROM LagunEgin WHERE Erabiltzailea1 = 'james@gmail.com'")
+        self.assertEqual(0, res3[0][0])  # 0 da, ez delako onartu
+
+    # Creo que hay que comprobar en la consulta que no se esté agregando a si mismo
