95af2d027ea430c88f9615ad555dd10bb9189405
Markel15 <enemar.h@gmail.com>
2023-12-28 20:12:13 +0100

    Arreglo de las fechas

--- model/Erreserbatuta.py
+++ model/Erreserbatuta.py
@@ -7,9 +7,9 @@ db = Connection()
 class Erreserbatuta:
 	def __init__(self, erabiltzaile, data, liburuId, entregaData):
 		self.erabiltzaile=erabiltzaile
-		self.erresData =datetime.fromtimestamp(data/1000).strftime("%d/%m/%Y")
+		self.erresData =datetime.fromtimestamp(data).strftime("%d/%m/%Y")
 		self.libId = liburuId
-		self.entrData = datetime.fromtimestamp(entregaData/1000).strftime("%d/%m/%Y")
+		self.entrData = datetime.fromtimestamp(entregaData).strftime("%d/%m/%Y")
 
 	def __str__(self):
 		return f"{self.erabiltzaile} ({self.libId})"
