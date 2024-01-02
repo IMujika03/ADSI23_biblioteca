4ce735717080b688221e1284c225ae86161ba0b3
ABenito03 <aitorbenito03@gmail.com>
2024-01-02 19:52:04 +0100

    test de consultar tema y comentarios bien

--- tests/test_foroak.py
+++ tests/test_foroak.py
@@ -6,15 +6,23 @@ class TestForoak(BaseTestClass):
         res = self.client.get('/foroak')
         self.assertEqual(200, res.status_code)  # Orriak ondo funtzionatzen du bertara heltzen delako
         page = BeautifulSoup(res.data, features="html.parser")
+        titles = [a.get_text(strip=True) for a in page.select('.container ul li a')]
+        self.assertIn('Pasahitza ahastuta?', titles)
+        self.assertIn('Nola logeatu', titles)
 
     def test_komentarioak_kontsultatu(self):
-        gaia_id = 1
+        gaia_id = 2
         res = self.client.get(f'/gaia?id={gaia_id}')
         self.assertEqual(200, res.status_code)
         page = BeautifulSoup(res.data, features="html.parser")
+        komentarioak = [li.get_text(strip=True) for li in page.select('.container ul li')]
+        self.assertIn('admin@gmail.com komentatu du: Sartu login atalean, ez du zailtasunik', komentarioak)
+        self.assertIn('jhon@gmail.com komentatu du: Eskerrik asko!!!', komentarioak)
 
     def test_komentatu(self):
         self.login('james@gmail.com', '123456')
         gaia_id = 1
         komentarioa_string = "Testaren proba"
         res = self.client.post(f'/gaia?id={gaia_id}', data={'komentarioa': komentarioa_string})
+        self.assertEqual(200, res.status_code)
+        page = BeautifulSoup(res.data, features="html.parser")
